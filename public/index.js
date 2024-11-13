2a1538bc4ba714896a7966926a80c7e9c79b0390
nicollasbispo <nicollas.bpereira@sptech.school>
2024-11-13 13:53:23 -0300

    api

---
+++ public/index.js
@@ -0,0 +1,136 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.second-section-img', {
+    duration: 1000,
+    distance: '200px',
+    delay:400,
+    origin: 'right'
+
+});
+
+sr.reveal('.texto', {
+    duration: 700,
+    distance: '200px',
+    delay:200,
+    origin: 'left'
+
+});
+
+sr.reveal('.box', {
+    duration: 2000,
+    distance: '90px'
+
+});
+
+
+sr.reveal('.h2', {
+    duration: 700,
+    distance: '90px',
+    delay:200,
+    origin: 'top'
+
+});
+
+
+
+//SEÇÃO BOXES (MISSÃO/VISÃO/VALORES)
+sr.reveal('.box1', {
+    duration: 1000,
+    distance: '90px',
+    delay: 200
+
+});
+
+sr.reveal('.box2', {
+    duration: 1000,
+    distance: '90px',
+    delay: 400
+
+});
+
+sr.reveal('.box3', {
+    duration: 1000,
+    distance: '90px',
+    delay: 600
+
+});
+
+//SEÇÃO BENEFICIOS
+sr.reveal('.beneficios', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'left',
+    delay: 400
+
+});
+
+
+sr.reveal('.atendimento', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'left',
+    delay: 400
+
+});
+
+sr.reveal('.img-atendimento' , {
+    duration: 1000,
+    distance: '200px',
+    origin: 'right',
+    delay: 500
+})
+
+sr.reveal('.img-beneficios' , {
+    duration: 1000,
+    distance: '200px',
+    origin: 'left',
+    delay: 500
+})
+
+
+//SEÇÃO SERVIÇOS
+sr.reveal('.passo1', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'left',
+    delay: 200
+
+});
+
+sr.reveal('.passo15', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'right',
+    delay: 400
+
+});
+
+sr.reveal('.passo2', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'left',
+    delay: 200
+
+});
+
+sr.reveal('.passo3', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'right',
+    delay: 400
+
+});
+
+sr.reveal('.passo4', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'left',
+    delay: 200
+
+});
+
+
+
+
+
+
